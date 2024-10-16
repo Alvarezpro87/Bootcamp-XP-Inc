787e5118ffd574e52a42049703f2619d1c2e8ce1
Alvarezpro87 <alvareznuvem@gmail.com>
2024-10-16 17:55:27 -0300

    Atualizando o readme

--- Projeto carro/minimal-api/API/obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ Projeto carro/minimal-api/API/obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2282c5928653f2cfcfe8282f13ec19260fe692fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c811b2a2b85e4af5d9180865e03469dec641e6e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
